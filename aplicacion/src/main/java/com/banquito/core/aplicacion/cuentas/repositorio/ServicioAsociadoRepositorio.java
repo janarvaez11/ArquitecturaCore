4243277b4d350fa7d10668ad320c2bdb4240e2b2
Ariel Narvaez <janarvaez11@espe.edu.ec>
2025-05-24 16:01:13 -0500

    Agregado Tabla ServicioAsociado y TipoServicioCuenta

---
+++ aplicacion/src/main/java/com/banquito/core/aplicacion/cuentas/repositorio/ServicioAsociadoRepositorio.java
@@ -0,0 +1,11 @@
+package com.banquito.core.aplicacion.cuentas.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.banquito.core.aplicacion.cuentas.modelo.ServicioAsociado;
+
+public interface ServicioAsociadoRepositorio extends JpaRepository<ServicioAsociado, Integer> {
+    
+    // Aquí puedes agregar métodos personalizados si es necesario
+
+}
