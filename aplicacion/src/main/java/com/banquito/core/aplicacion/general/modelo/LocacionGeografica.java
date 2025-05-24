18c24b74912e84e33926bc3cecc7561870c716a0
Wagner Cuenca <wacuenca@espe.edu.ec>
2025-05-24 16:29:09 -0500

    Creacion/ LocacionGeografica

--- aplicacion/src/main/java/com/banquito/core/aplicacion/general/modelo/LocacionGeografica.java
+++ aplicacion/src/main/java/com/banquito/core/aplicacion/general/modelo/LocacionGeografica.java
@@ -45,5 +45,122 @@ public class LocacionGeografica {
     public LocacionGeografica() {
     }
 
+    public LocacionGeografica(Integer id, LocacionGeografica locacionPadre) {
+        this.id = id;
+        this.locacionPadre = locacionPadre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocacionGeografica getLocacionPadre() {
+        return locacionPadre;
+    }
+
+    public void setLocacionPadre(LocacionGeografica locacionPadre) {
+        this.locacionPadre = locacionPadre;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public Integer getCodigoNivel() {
+        return codigoNivel;
+    }
+
+    public void setCodigoNivel(Integer codigoNivel) {
+        this.codigoNivel = codigoNivel;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigoTelefonoArea() {
+        return codigoTelefonoArea;
+    }
+
+    public void setCodigoTelefonoArea(String codigoTelefonoArea) {
+        this.codigoTelefonoArea = codigoTelefonoArea;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public List<LocacionGeografica> getLocacionesHijas() {
+        return locacionesHijas;
+    }
+
+    public void setLocacionesHijas(List<LocacionGeografica> locacionesHijas) {
+        this.locacionesHijas = locacionesHijas;
+    }
+
+    public EstructuraGeografica getEstructuraGeografica() {
+        return estructuraGeografica;
+    }
+
+    public void setEstructuraGeografica(EstructuraGeografica estructuraGeografica) {
+        this.estructuraGeografica = estructuraGeografica;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((locacionPadre == null) ? 0 : locacionPadre.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LocacionGeografica other = (LocacionGeografica) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (locacionPadre == null) {
+            if (other.locacionPadre != null)
+                return false;
+        } else if (!locacionPadre.equals(other.locacionPadre))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "LocacionGeografica [id=" + id + ", locacionPadre=" + locacionPadre + ", pais=" + pais + ", codigoNivel="
+                + codigoNivel + ", nombre=" + nombre + ", codigoTelefonoArea=" + codigoTelefonoArea + ", codigoPostal="
+                + codigoPostal + ", locacionesHijas=" + locacionesHijas + ", estructuraGeografica="
+                + estructuraGeografica + "]";
+    }
+
+    
 
 }
