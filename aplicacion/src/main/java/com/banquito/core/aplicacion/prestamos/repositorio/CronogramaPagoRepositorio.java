87bcf3f2e57453f9c22a741c97af278159c7a86a
Angelo Andres Olmedo Camacho <aaolmedo2@espe.edu.ec>
2025-05-24 20:38:33 -0500

    Servicios prestamos
    Create four services of prestamos, with basic methods.

--- aplicacion/src/main/java/com/banquito/core/aplicacion/prestamos/repositorio/CronogramaPagoRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.banquito.core.aplicacion.prestamos.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.banquito.core.aplicacion.prestamos.modelo.CronogramasPagos;
-
-public interface CronogramaPagoRepositorio extends JpaRepository<CronogramasPagos, Integer> {
-}
