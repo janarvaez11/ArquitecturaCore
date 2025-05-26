915ccb620594de6225d421ef61db0c74a060896a
Angelo Andres Olmedo Camacho <aaolmedo2@espe.edu.ec>
2025-05-26 16:05:40 -0500

    Loans reload

--- aplicacion/src/main/java/com/banquito/core/aplicacion/prestamos/servicio/TipoPrestamosServicio.java
+++ aplicacion/src/main/java/com/banquito/core/aplicacion/prestamos/servicio/TipoPrestamosServicio.java
@@ -1,7 +1,6 @@
 package com.banquito.core.aplicacion.prestamos.servicio;
 
 import java.util.Optional;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
 import java.util.Arrays;
