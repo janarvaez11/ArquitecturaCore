d7e48cffb85841da797c7343a6ca0d849e588363
Nayeli Michelle Tipantiza Cumbal <nmtipantiza@espe.edu.ec>
2025-06-01 19:39:38 -0500

    Repository correction

--- aplicacion/src/main/java/com/banquito/core/aplicacion/cuentas/repositorio/TasaInteresRepositorio.java
+++ aplicacion/src/main/java/com/banquito/core/aplicacion/cuentas/repositorio/TasaInteresRepositorio.java
@@ -3,25 +3,15 @@ package com.banquito.core.aplicacion.cuentas.repositorio;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.banquito.core.aplicacion.cuentas.modelo.TasaInteres;
 
-import jakarta.persistence.QueryHint;
-
 @Repository
 public interface TasaInteresRepositorio extends JpaRepository<TasaInteres, Integer> {
-    
-    @Query("SELECT DISTINCT t FROM TasaInteres t JOIN TipoCuenta tc ON t.IdTasaInteres = tc.tasaInteres.IdTasaInteres WHERE tc.IdTipoCuenta = :idTipoCuenta")
-    List<TasaInteres> findByTipoCuentaId(@Param("idTipoCuenta") Integer idTipoCuenta);
-    
-    @Query("SELECT t FROM TasaInteres t JOIN TasaSaldo ts ON t.IdTasaInteres = ts.tasaInteres.IdTasaInteres WHERE ts.SaldoMinimo >= :saldoMinimo AND ts.SaldoMaximo <= :saldoMaximo")
-    List<TasaInteres> findByRangoSaldo(@Param("saldoMinimo") Double saldoMinimo, @Param("saldoMaximo") Double saldoMaximo);
-    
-    @Query("SELECT t FROM TasaInteres t WHERE t.Estado = :estado")
-    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
-    List<TasaInteres> findByEstado(@Param("estado") String estado);
-} 
+    List<TasaInteres> findByTipoCuenta_IdTipoCuenta(Integer idTipoCuenta);
+    List<TasaInteres> findByValorMinimoBetweenOrValorMaximoBetween(
+        Double valorMinimo1, Double valorMinimo2, 
+        Double valorMaximo1, Double valorMaximo2);
+    List<TasaInteres> findByEstado(String estado);
+}
